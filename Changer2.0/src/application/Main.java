0e0f58e2692c401eea187eea2d4f4fb1f2fd079f
user <user@ABS238.in.ab-soft.net>
2017-04-04 09:36:45 +0300

    adding comment

--- Changer2.0/src/application/Main.java
+++ Changer2.0/src/application/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
 		Scene scene = new Scene(root,primaryStage.getWidth(), primaryStage.getHeight());
 		
 		
-		
+		//attempt to launch
 		
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("First Window");
