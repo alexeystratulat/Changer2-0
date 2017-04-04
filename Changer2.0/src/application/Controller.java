44671a63f13220eb6d89adda3b0ea8da923a4dab
user <user@ABS238.in.ab-soft.net>
2017-04-04 15:14:39 +0300

    closing Window

--- Changer2.0/src/application/Controller.java
+++ Changer2.0/src/application/Controller.java
@@ -1,16 +1,23 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.InputEvent;
+import javafx.stage.Stage;
 
 public class Controller {
-	
+
 	@FXML
-	public void nextButtonClicked(){
+	public void nextButtonClicked() {
 		System.out.println("NEXT");
-		
-		
-		
-		
+
 	}
 
+	@FXML
+	public void onMouseClickedCancelBtn(InputEvent e) {
+		final Node source = (Node) e.getSource();
+		final Stage stage = (Stage) source.getScene().getWindow();
+		stage.close();
+
+	}
 }
