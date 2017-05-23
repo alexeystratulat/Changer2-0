96240d33b5f214d2523b9e3e309ffc64680ce978
user <user@ABS238.in.ab-soft.net>
2017-05-23 12:17:15 +0300

    restarting window - beginning

---
+++ Changer2.0/src/application/RestartingWindowForReboot.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.fxml.FXML;
+
+public class RestartingWindowForReboot {
+	
+	
+	
+	
+	@FXML
+	private void initialize() {
+		
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
